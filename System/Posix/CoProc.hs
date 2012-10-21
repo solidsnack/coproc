20678718ba38158a74bec995e8f06572562c5eee
Jason Dusek <jason.dusek@gmail.com>
2012-10-21 21:49:26 +0000

    Reorganization of code. More robust 'close'.

--- System/Posix/CoProc.hs
+++ System/Posix/CoProc.hs
@@ -4,26 +4,23 @@
 module System.Posix.CoProc where
 
 import           Control.Applicative
-import           Control.Concurrent
 import           Control.Concurrent.MVar
-import           Control.Exception
-import           Control.Monad
-import           Data.Bits
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as Bytes
-import           Data.Maybe
 import           Data.Monoid
 import           System.Exit
 import           System.IO
 import           System.Process
 import           System.Posix.ByteString
 
+import qualified Text.ShellEscape as Esc
+
 import qualified System.Posix.CoProc.Internals as Internals
 
 
 start :: (Internals.Interpreter int) => int -> IO (Session int)
 start interpreter = do
-  handles@(_,_,_,p) <- Internals.boot interpreter
+  handles@(_,_,_,p) <- Internals.start interpreter
   pid               <- Internals.pid p
   Session pid interpreter handles <$> newMVar ()
 
@@ -31,10 +28,10 @@ query :: (Internals.Interpreter int) => Session int
       -> ByteString
       -> IO (ByteString, ByteString)
 query Session{..} q = withMVar lock
-                               (const (Internals.send interpreter handles q))
+                               (const (Internals.query interpreter handles q))
 
 done :: (Internals.Interpreter int) => Session int -> IO (CPid, ExitCode)
-done Session{..} = (pid,) <$> Internals.end interpreter handles
+done Session{..} = (pid,) <$> Internals.done interpreter handles
 
 data Session int = Session { pid :: CPid
                            , interpreter :: int
@@ -44,4 +41,21 @@ instance (Show int) => Show (Session int) where
   show Session{..} = unwords [ "Session", "pid=" <> show pid
                                         , "interpreter=" <> show interpreter
                                         , "[lock]", "[handles]" ]
+
+
+-- | Bash as a coprocess.
+data Bash = Bash
+instance Show Bash where show _ = "bash"
+
+instance Internals.Interpreter Bash where
+  start _ = runInteractiveProcess "bash" [] Nothing (Just [])
+  done  _ = Internals.close
+  query _ (i, _, _, _) cmd = Internals.withFIFOs query'
+   where query' ofo efo = do
+           Bytes.hPut i (Bytes.unlines wrapped)
+           hFlush i
+           [ob, eb] <- Internals.backgroundReadFIFOs [ofo, efo]
+           return (ob, eb)
+          where wrapped = [ "{", cmd, "} 1>" <> (Esc.bytes . Esc.bash) ofo <>
+                                       " 2>" <> (Esc.bytes . Esc.bash) efo    ]
 
--- System/Posix/CoProc/Internals.hs
+++ System/Posix/CoProc/Internals.hs
@@ -6,15 +6,10 @@ module System.Posix.CoProc.Internals where
 
 import           Control.Applicative
 import           Control.Concurrent
-import           Control.Concurrent.MVar
-import           Control.Exception
-import           Control.Monad
 import           Data.Bits
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as Bytes
-import           Data.Maybe
 import           Data.Monoid
-import qualified GHC.IO.Handle.FD
 import           System.Exit
 import           System.IO
 import           System.IO.Error
@@ -23,36 +18,24 @@ import           System.Process.Internals
 import           System.Posix.ByteString
 
 import           System.IO.Temp
+
 import qualified Text.ShellEscape as Esc
 
 
+-- | The backend interface, which provides the core of the frontend 'start',
+--   'query' and 'done' operations.
 class Interpreter interpreter where
-  boot :: interpreter -> IO (Handle, Handle, Handle, ProcessHandle)
-  send :: interpreter -> (Handle, Handle, Handle, ProcessHandle)
-       -> ByteString -> IO (ByteString, ByteString)
-  end  :: interpreter -> (Handle, Handle, Handle, ProcessHandle) -> IO ExitCode
-
-data Bash = Bash
-instance Show Bash where show _ = "bash"
-
-instance Interpreter Bash where
-  boot _ = runInteractiveProcess "bash" [] Nothing (Just [])
-  end  _ = close
-  send _ (i, _, _, _) cmd = withFIFOs query'
-   where query' ofo efo = do
-           Bytes.hPut i (Bytes.unlines wrapped)
-           hFlush i
-           [ob, eb] <- backgroundReadFIFOs [ofo, efo]
-           return (ob, eb)
-          where wrapped = [ "{", cmd, "} 1>" <> (Esc.bytes . Esc.bash) ofo <>
-                                       " 2>" <> (Esc.bytes . Esc.bash) efo    ]
-
+  start :: interpreter -> IO (Handle, Handle, Handle, ProcessHandle)
+  query :: interpreter -> (Handle, Handle, Handle, ProcessHandle)
+                       -> ByteString -> IO (ByteString, ByteString)
+  done  :: interpreter -> (Handle, Handle, Handle, ProcessHandle) -> IO ExitCode
 
 -- | Simplest way to shutdown a running interpreter.
 close :: (Handle, Handle, Handle, ProcessHandle) -> IO ExitCode
-close (i, _, _, p) = hClose i *> waitForProcess p
+close (i, o, e, p) = hClose' i *> hClose' o *> hClose' e *> waitForProcess p
+ where hClose' h = catchIOError (hClose h) (const (return ()))
 
--- | Run an IO action with two FIFOs in scope, which will removed after it
+-- | Run an IO action with two FIFOs in scope, which will be removed after it
 --   completes.
 withFIFOs :: (RawFilePath -> RawFilePath -> IO a) -> IO a
 withFIFOs m = withSystemTempDirectory "hs.coproc." m'
@@ -71,11 +54,17 @@ drainFIFO path = do
   hClose e
   Bytes.hGetContents o <* waitForProcess p
 
+-- | Read several FIFOs in the background, in parallel, returning all of their
+--   contents.
+backgroundReadFIFOs :: [ByteString] -> IO [ByteString]
 backgroundReadFIFOs theFIFOs = do
   cells <- sequence (newEmptyMVar <$ theFIFOs)
   sequence_ [ forkIO (drainFIFO p >>= putMVar c) | p <- theFIFOs | c <- cells ]
   sequence (takeMVar <$> cells)
 
+-- | Retrieve the PID from a process handle. Note that calling this function
+--   on a process that one has terimated or called wait for will results in an
+--   exception being thrown.
 pid :: ProcessHandle -> IO CPid
 pid handle = withProcessHandle handle getPID
  where getPID h@(OpenHandle pid) = return (h, pid)
