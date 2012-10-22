c20a76352d265a421d147a7e2dcb0e2209ed3e44
Jason Dusek <jason.dusek@gmail.com>
2012-10-22 10:00:19 +0000

    Clean up for release.

--- System/Posix/CoProc.hs
+++ System/Posix/CoProc.hs
@@ -1,32 +1,29 @@
 {-# LANGUAGE OverloadedStrings
            , RecordWildCards
            , TupleSections #-}
-module System.Posix.CoProc where
+module System.Posix.CoProc ( start, query, done, Session(..)
+                           , Bash(..), BinSh(..), BourneLike(..) ) where
 
 import           Control.Applicative
 import           Control.Concurrent.MVar
 import           Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as Bytes
 import           Data.Monoid
 import           System.Exit
 import           System.IO
 import           System.Process
 import           System.Posix.ByteString
 
-import qualified Text.ShellEscape as Esc
-
 import qualified System.Posix.CoProc.Internals as Internals
+import           System.Posix.CoProc.Shell
 
 
 start :: (Internals.Interpreter int) => int -> IO (Session int)
-start interpreter = do
-  handles@(_,_,_,p) <- Internals.start interpreter
-  pid               <- Internals.pid p
-  Session pid interpreter handles <$> newMVar ()
+start interpreter = do handles@(_,_,_,p) <- Internals.start interpreter
+                       pid               <- Internals.pid p
+                       Session pid interpreter handles <$> newMVar ()
 
-query :: (Internals.Interpreter int) => Session int
-      -> ByteString
-      -> IO (ByteString, ByteString)
+query :: (Internals.Interpreter int)
+      => Session int -> ByteString -> IO (ByteString, ByteString)
 query Session{..} q = (withMVar lock . const)
                       (Internals.query interpreter handles q)
 
@@ -38,30 +35,13 @@ done Session{..} = (withMVar lock . const)
 --   interpreter as well as a lock. The 'start', 'query' and 'done'
 --   functions work with 'Session's to ensure the thread safety of
 --   interactions of a running interpreter.
-data Session int = Session { pid :: CPid
-                           , interpreter :: int
-                           , handles :: (Handle, Handle, Handle, ProcessHandle)
-                           , lock :: MVar() }
+data Session int = Session
+  { pid         :: CPid
+  , interpreter :: int
+  , handles     :: (Handle, Handle, Handle, ProcessHandle)
+  , lock        :: MVar() }
 instance (Show int) => Show (Session int) where
   show Session{..} = unwords [ "Session", "pid=" <> show pid
                                         , "interpreter=" <> show interpreter
                                         , "[lock]", "[handles]" ]
-
-
--- | Bash as a coprocess. Simply starts @bash@ with no arguments, using
---   whatever @bash@ is first in the @PATH@.
-data Bash = Bash
-instance Show Bash where show _ = "bash"
-
-instance Internals.Interpreter Bash where
-  start _ = runInteractiveProcess "bash" [] Nothing (Just [])
-  done  _ = Internals.close
-  query _ (i, _, _, _) cmd = Internals.withFIFOs query'
-   where query' ofo efo = do
-           Bytes.hPut i (Bytes.unlines wrapped)
-           hFlush i
-           [ob, eb] <- Internals.backgroundReadFIFOs [ofo, efo]
-           return (ob, eb)
-          where wrapped = [ "{", cmd, "} 1>" <> (Esc.bytes . Esc.bash) ofo <>
-                                       " 2>" <> (Esc.bytes . Esc.bash) efo    ]
 
---
+++ System/Posix/CoProc/Shell.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | CoProc implementations for shells.
+module System.Posix.CoProc.Shell where
+
+import           Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as Bytes
+import           Data.Monoid
+import           System.IO
+import           System.Process
+
+import qualified Text.ShellEscape as Esc
+
+import qualified System.Posix.CoProc.Internals as Internals
+
+
+-- | Bash as a coprocess. Simply starts @bash@ with no arguments, using
+--   whatever @bash@ is first in the @PATH@.
+data Bash = Bash
+instance Show Bash where show _ = "bash"
+instance Internals.Interpreter Bash where
+  start _ = go "bash"
+  done  _ = Internals.close
+  query _ handles cmd = runRedirected handles cmd
+
+-- | Sh as a coprocess. Starts @/bin/sh@ with no arguments.
+data BinSh = BinSh
+instance Show BinSh where show _ = "/bin/sh"
+instance Internals.Interpreter BinSh where
+  start _ = go "/bin/sh"
+  done  _ = Internals.close
+  query _ handles cmd = runRedirected handles cmd
+
+-- | Launch any Bourne-compatible shell as a coprocess.
+data BourneLike = BourneLike String
+instance Show BourneLike where show (BourneLike path) = path
+instance Internals.Interpreter BourneLike where
+  start (BourneLike p) = go p
+  done  _ = Internals.close
+  query _ handles cmd = runRedirected handles cmd
+
+
+-- | Redirecting wrapper for Bourne-compatible shell.
+redirect :: (ByteString, ByteString) -> ByteString -> ByteString
+redirect (o,e) c = Bytes.unlines [ "{", c, "} 1>" <> (Esc.bytes . Esc.sh) o <>
+                                            " 2>" <> (Esc.bytes . Esc.sh) e    ]
+
+-- | Setup FIFOs and run a command, capturing its output.
+runRedirected (i,_,_,_) cmd = Internals.withFIFOs query'
+   where query' ofo efo = do Bytes.hPut i (redirect (ofo,efo) cmd)
+                             hFlush i
+                             [ob,eb] <- Internals.backgroundReadFIFOs [ofo,efo]
+                             return (ob,eb)
+
+-- | Launch a shell with no arguments.
+go shell = runInteractiveProcess shell [] Nothing (Just [])
+
